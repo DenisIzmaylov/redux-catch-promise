67f12a8fd46bb2e3aa4ab2d6391d6f5b52f33298
Denis Izmaylov <izmaylov.dm@gmail.com>
2015-08-10 14:31:27 +0300

    fix source

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-export default function PromisedThunk(callback) {
+export default function catchPromise(callback) {
   return (store) => (next) => (action) => {
     const { dispatch, getState } = store;
-    if (typeof action === 'function') {
-      const result = action(dispatch, getState);
-      if (typeof result === 'object' && typeof result.then === 'function') {
+    if (typeof action === 'object' && typeof action.then === 'function') {
+      if (typeof callback === 'function') {
         callback(result, action, store);
       }
-      return result;
     }
     return next(action);
   };
